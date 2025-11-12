c19499696f3874f443e5c5f3172b9be8a3e04d88
Thijs Teerhuis <thijs@teerhuis.eu>
2025-11-12 10:21:19 +0100

    Code convention.

---
+++ include/core/Networking/Transport.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <cstddef>
+#include <functional>
+
+
+enum class SendMode {
+    ReliableOrdered,
+    ReliableUnordered,
+    Unreliable
+};
+
+
+struct TransportResult {
+    bool success;
+    int errorCode;
+};
+
+
+using OnMessageReceivedCallback = std::function<void(int connectionId, const std::byte* data, size_t length)>;
+using OnConnectionChangedCallback = std::function<void(int connectionId, bool connected)>;
+
+class Transport {
+public:
+	virtual ~Transport() = default;
+
+	virtual TransportResult startServer(uint16_t port) = 0;
+	virtual TransportResult startClient(const char* serverAddress, uint16_t port) = 0;
+	virtual TransportResult send(int connectionId, const std::byte data[], size_t length, bool reliable) = 0;
+	virtual TransportResult disconnect(int connectionId) = 0;
+	virtual void poll() = 0;
+	virtual void shutdown() = 0;
+
+	void setOnMessageReceived(OnMessageReceivedCallback callback) { onMessageReceived = callback; }
+	void setOnConnectionChanged(OnConnectionChangedCallback callback) { onConnectionChanged = callback; }
+
+protected:
+	OnMessageReceivedCallback onMessageReceived;
+	OnConnectionChangedCallback onConnectionChanged;
+};
