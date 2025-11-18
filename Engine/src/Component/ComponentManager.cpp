bf90386b0d6562dfb1e8cbf80b4048befe1caeb1
SAMBA-PC\samle <sam.leguijt@gmail.com>
2025-11-18 16:44:15 +0100

    Fixed includes in .cpp files

--- Engine/src/Component/ComponentManager.cpp
+++ Engine/src/Component/ComponentManager.cpp
@@ -2,9 +2,9 @@
 // Created by samle on 10/11/2025.
 //
 
-#include "../../../Engine/inc/component/ComponentManager.h"
-#include "../../../Engine/inc/GameObject/GameObject.h"
-#include "../../../Engine/inc/component/Component.h"
+#include "Component/ComponentManager.h"
+#include "GameObject/GameObject.h"
+#include "Component/Component.h"
 
 
 ComponentManager::~ComponentManager()
--- Engine/src/Component/Transform.cpp
+++ Engine/src/Component/Transform.cpp
@@ -1,4 +1,4 @@
-#include "../../inc/Component/Transform.h"
+#include "Component/Transform.h"
 
 Transform::Transform(const Vector2 position, const double rotationAngle, const Vector2 scale) :
     position(position), rotationAngle(rotationAngle), scale(scale){}
