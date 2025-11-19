f301ba675b5b3096d00f23edc80c6e47d0144903
Sirbarry <lieven3.schokker@gmail.com>
2025-11-19 18:28:23 +0100

    Fix: changed raw message

--- Engine/inc/Networking/Messages/IncommingRawMessage.h
+++ Engine/inc/Networking/Messages/IncommingRawMessage.h
@@ -4,17 +4,12 @@
 
 struct IncomingRawMessage
 {
-    int connectionID;
-    const std::byte* data;
-    size_t length;
-
     IncomingRawMessage(int connID, const std::byte* ptr, size_t len)
         : connectionID(connID), data(ptr), length(len)
     {
     }
 
-    std::string_view asStringView() const
-    {
-        return std::string_view(reinterpret_cast<const char*>(data), length);
-    }
+    int connectionID;
+    const std::byte* data;
+    size_t length;
 };
