930ed5eba7957e2b34f18affe7959ddf793b5022
Sirbarry <lieven3.schokker@gmail.com>
2025-11-21 14:59:56 +0100

    Fix: Test no  Threading

--- Tests/Core/SpelMotorTest.cpp
+++ Tests/Core/SpelMotorTest.cpp
@@ -9,39 +9,24 @@
 
 class SpelMotorTest : public ::testing::Test
 {
+public:
+    ApplicationSpecifications specifications{};
 protected:
     void SetUp() override
     {
-        // Set environment variable - this takes precedence over everything
-#ifdef _WIN32
-        _putenv_s("SDL_VIDEODRIVER", "dummy");
-#else
-        setenv("SDL_VIDEODRIVER", "dummy", 1);
-#endif
-
         if (SDL_Init(SDL_INIT_EVENTS | SDL_INIT_VIDEO) < 0)
         {
             FAIL() << "SDL_Init failed: " << SDL_GetError();
         }
-
         specifications.windowOptions = {"Test Window", 800, 600};
         specifications.renderBackend = RenderBackend::SDL;
     }
 
     void TearDown() override
     {
-#ifdef _WIN32
-        _putenv_s("SDL_VIDEODRIVER", "");
-#else
-        unsetenv("SDL_VIDEODRIVER");
-#endif
         SDL_Quit();
     }
 
-    // Member variables
-    ApplicationSpecifications specifications {};
-
-    // Helper function to push quit event after delay
     static void pushQuitEventAfterDelay(int milliseconds)
     {
         std::thread([milliseconds]()
@@ -64,10 +49,8 @@ TEST_F(SpelMotorTest, CleanSubsystemShutdownViaQuitEvent)
     //Arrange
     auto* engine = new SpelMotor(specifications);
 
-#ifdef __APPLE__
-    // On macOS, even with dummy driver, it's safer to run on main thread
-    // Push quit event from another thread
-    std::thread quitThread([]() {
+    std::thread quitThread([]()
+    {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         SDL_Event quitEvent;
         quitEvent.type = SDL_QUIT;
@@ -77,21 +60,7 @@ TEST_F(SpelMotorTest, CleanSubsystemShutdownViaQuitEvent)
 
     //Act - Run on main thread
     engine->run();
-#else
-    // Other platforms can safely use threads
-    pushQuitEventAfterDelay(100);
 
-    //Act
-    std::thread engineThread([engine]()
-    {
-        engine->run();
-    });
-
-    if (engineThread.joinable())
-    {
-        engineThread.join();
-    }
-#endif
 
     //Assert
     ASSERT_NO_THROW({
