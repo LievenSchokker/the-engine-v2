6f37baba3abed098b3855a3adfebac3b83024639
TotallyOriginalUsername <mae.korthout@student.avans.nl>
2025-11-22 14:56:47 +0100

    feat: Added UIObjects

---
+++ Engine/inc/Component/UIObject/UIObject.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Component/Component.h"
+
+
+class UIObject: public Component
+{
+   public:
+	explicit UIObject(int width = 0, int height = 0);
+	~UIObject() override;
+
+	int getWidth() const;
+	int getHeight() const;
+
+	void setWidth(const int newWidth);
+	void setHeight(const int newHeight);
+
+   private:
+	int width;
+	int height;
+};
