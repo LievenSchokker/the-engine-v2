72d3605c54a106c54396c80aac491d90e35f7f82
Sirbarry <lieven3.schokker@gmail.com>
2025-11-22 17:23:02 +0100

    Fix: Simplified MessageWriter

--- Engine/src/Networking/Messages/MessageWriter.cpp
+++ Engine/src/Networking/Messages/MessageWriter.cpp
@@ -1,38 +1,21 @@
-// MessageWriter.cpp
-#include "Networking/Messages/MessageWriter.h"
+#pragma once
 
+
+#include "Networking/Messages/MessageWriter.h"
 #include "Networking/Messages/IMessage.h"
+#include "Networking/Messages/ConnectionMessage.h"
 #include "Networking/Messages/OutgoingRawMessage.h"
 #include "Networking/SendMode.h"
+
+
 #include <cstring>
+
 
 OutgoingRawMessage MessageWriter::writeMessage(
     const IMessage& message,
     int connectionId,
     SendMode sendMode)
 {
-    std::vector<std::byte> packet = buildPacket(message);
-    return {connectionId, std::move(packet), sendMode};
+    return {connectionId,  message.serialize(), sendMode};
 }
 
-std::vector<std::byte> MessageWriter::buildPacket(const IMessage& message)
-{
-    MessageTypes messageType = message.getMessageType();
-    auto typeValue = static_cast<uint8_t>(messageType);
-
-
-    std::vector<std::byte> payload = message.serialize();
-
-    std::vector<std::byte> packet(sizeof(uint8_t) + payload.size());
-
-    std::memcpy(packet.data(), &typeValue, sizeof(uint8_t));
-
-    if (!payload.empty())
-    {
-        std::memcpy(packet.data() + sizeof(uint8_t),
-                    payload.data(),
-                    payload.size());
-    }
-
-    return packet;
-}
