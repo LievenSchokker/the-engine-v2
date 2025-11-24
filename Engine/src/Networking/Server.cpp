aa57d1789728aa4623eaf5f51bbbe1a37bad0516
Sirbarry <lieven3.schokker@gmail.com>
2025-11-24 20:46:08 +0100

    Fix: small bug

--- Engine/src/Networking/Server.cpp
+++ Engine/src/Networking/Server.cpp
@@ -168,7 +168,7 @@ bool Server::sendMessage(const int clientId, const IMessage& message, const Send
 
 bool Server::sendMessage(const int clientId, const IMessage& message) const
 {
-    return sendMessage(clientId, message, SendMode::ReliableOrdered);
+    return sendMessage(clientId, message, SendMode::Unreliable);
 }
 
 bool Server::broadcastMessage(const IMessage& message) const
