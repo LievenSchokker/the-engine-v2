71cff35ed2c95382def2aff4bffa59fb358af574
Thijs Teerhuis <thijs@teerhuis.eu>
2025-11-24 22:04:18 +0100

    Lokal physics implemented
    - Also implemented rigid body and collider
    - Cleaned up previeus implementation.

--- Engine/inc/Physics/Components/Collider.h
+++ Engine/inc/Physics/Components/Collider.h
@@ -1,6 +1,33 @@
+//
+// Created by thijs on 24-11-2025.
+//
+
+
+#pragma once
 #include "Component/Component.h"
+#include "Physics/PhysicsShapes.h"
 
-class Collider : Component
+
+class Collider: public Component
 {
+public:
+	PhysicsShapeType shape = PhysicsShapeType::None;
 
+	float radius = 0.0f; // circle
+	Vector2 size = {0, 0}; // box
+
+	float density = 1.0f;
+	bool isSensor = false;
+
+	void setCircle(const float radiusNew)
+	{
+		shape = PhysicsShapeType::Circle;
+		radius = radiusNew;
+	}
+
+	void setRectangle(const Vector2& fullSize)
+	{
+		shape = PhysicsShapeType::Rectangle;
+		size = fullSize;
+	}
 };
