ef672ebed6457bb1d3b776fd04e250938f2f5204
Pablo Sanderman <pablosanderman@gmail.com>
2025-11-28 16:01:46 +0100

    fix: Spelmotor using the wrong input system

--- Engine/src/Core/Spelmotor.cpp
+++ Engine/src/Core/Spelmotor.cpp
@@ -1,26 +1,26 @@
-#include "Core/ApplicationSpecifications.h"
 #include "Core/SpelMotor.h"
+
 #include "Core/ApplicationClock.h"
+#include "Core/ApplicationSpecifications.h"
 #include "External/SdlContext.h"
 #include "Input/InputManager.h"
 #include "Physics/Box2D/Box2DPhysicsWorld.h"
 #include "Rendering/IRenderer.h"
 #include "Rendering/SDL/SDLRenderer.h"
 
-
 SpelMotor::SpelMotor(ApplicationSpecifications const applicationSpecifications)
 	: running(false),
 	  specifications(applicationSpecifications),
 	  timer(nullptr),
 	  tickRate(applicationSpecifications.tickRate),
-	  physicsWorld(std::make_unique<Box2DPhysicsWorld>(applicationSpecifications.tickRate))
+	  physicsWorld(std::make_unique<Box2DPhysicsWorld>(
+		  applicationSpecifications.tickRate))
 {
-	if (applicationSpecifications.renderBackend == RenderBackend::SDL)
-	{
+	if ( applicationSpecifications.renderBackend == RenderBackend::SDL ) {
 		SdlContext context = SdlContext();
 		timer.reset();
 		timer = std::make_unique<ApplicationClock>(1.0f / tickRate, []() {
-			//Get Ticks returns ms we need seconds;
+			// Get Ticks returns ms we need seconds;
 			return (SDL_GetTicks() / 1000.0);
 		});
 
@@ -28,17 +28,15 @@ SpelMotor::SpelMotor(ApplicationSpecifications const applicationSpecifications)
 	}
 }
 
-
 SpelMotor::~SpelMotor() = default;
-
 
 void SpelMotor::run()
 {
 	timer->start();
 
-	//TODO Server or Client -> Start()
+	// TODO Server or Client -> Start()
 	physicsWorld->start();
-	//TODO SceneManager -> Start()
+	// TODO SceneManager -> Start()
 
 	renderer->open(specifications.windowOptions);
 
@@ -50,40 +48,35 @@ void SpelMotor::update()
 {
 	running = true;
 
-	while (running)
-	{
+	InputManager* input = InputManager::getInstance();
+
+	while ( running ) {
 		timer->tick();
 
-		//TODO REPLACE THIS WITH EVENTMANAGER
-		SDL_Event event;
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
-				shutdown();
-			}
+		if ( input->quitRequested() ) {
+			running = false;
 		}
 
-		while (timer->shouldFixedUpdate()) {
-			InputManager::getInstance()->update();
+		while ( timer->shouldFixedUpdate() ) {
+			input->update();
 			physicsWorld->update();
-
 			timer->consumeFixedUpdate();
 		}
 
-		//TODO Network->Update()
-		//TODO Audio->Update();
+		// TODO Network->Update()
+		// TODO Audio->Update();
 		renderer->presentFrame();
 	}
 }
-
 
 void SpelMotor::shutdown()
 {
 	running = false;
 
-	//TODO audioSystem->shutdown()
+	// TODO audioSystem->shutdown()
 	InputManager::shutdown();
 	renderer->close();
-	//TODO scenemanager->shutdown()
+	// TODO scenemanager->shutdown()
 	physicsWorld->shutdown();
-	//TODO server->shutdown() and client->shutdown()
+	// TODO server->shutdown() and client->shutdown()
 }
