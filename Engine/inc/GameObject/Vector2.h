58d42ec42cb257c556c75de6e623a16153322ce2
SAMBA-PC\samle <sam.leguijt@gmail.com>
2025-12-01 20:26:14 +0100

    Added operator overloads to Vector2.h

--- Engine/inc/GameObject/Vector2.h
+++ Engine/inc/GameObject/Vector2.h
@@ -1,10 +1,103 @@
 #pragma once
 
+#include <cmath>
 
 /**
  * @brief A struct for storing 2D vector points.
  */
-struct Vector2 {
-    double x;
-    double y;
+struct Vector2
+{
+    float x{};
+    float y{};
+
+    /// Adds each element of the other Vector to this Vector's respective element (v1.x + v2.x, v1.y + v2.y)
+    Vector2 operator+(const Vector2& other) const
+    {
+        return { x + other.x, y + other.y };
+    }
+
+    /// Subtracts each element of the other Vector to this Vector's respective element (v1.x - v2.x, v1.y - v2.y)
+    Vector2 operator-(const Vector2& other) const
+    {
+        return { x - other.x, y - other.y };
+    }
+
+    /// Multiplies each element of the other Vector to this Vector's respective element (v1.x * v2.x, v1.y * v2.y)
+    Vector2 operator*(const Vector2& other) const
+    {
+        return { x * other.x, y * other.y };
+    }
+
+    /// Divides each element of the other Vector to this Vector's respective element (v1.x / v2.x, v1.y / v2.y)
+    Vector2 operator/(const Vector2& other) const
+    {
+        return { x / other.x, y / other.y };
+    }
+
+
+    /// Multiplies this vector's component by a scalar value (v1.x * scalar, v1.y * scalar)
+    Vector2 operator*(float scalar) const
+    {
+        return { x * scalar, y * scalar };
+    }
+
+    /// Divides this vector's component by a scalar value (v1.x / scalar, v1.y / scalar)
+    Vector2 operator/(float scalar) const
+    {
+        return { x / scalar, y / scalar };
+    }
+
+    /// Adds each element of the other Vector to this Vector's respective element (v1.x + v2.x, v1.y + v2.y)
+    Vector2& operator+=(const Vector2& other)
+    {
+        x += other.x;
+        y += other.y;
+
+        return *this;
+    }
+
+    /// Subtracts each element of the other Vector to this Vector's respective element (v1.x - v2.x, v1.y - v2.y)
+    Vector2& operator-=(const Vector2& other)
+    {
+        x -= other.x;
+        y -= other.y;
+
+        return *this;
+    }
+
+    /// Multiplies this vector's component by a scalar value (v1.x * scalar, v1.y * scalar)
+    Vector2& operator*=(const Vector2& other)
+    {
+        x *= other.x;
+        y *= other.y;
+
+        return *this;
+    }
+
+    /// Divides this vector's component by a scalar value (v1.x / scalar, v1.y / scalar)
+    Vector2& operator/=(const Vector2& other)
+    {
+        x /= other.x;
+        y /= other.y;
+
+        return *this;
+    }
+
+    /// Multiplies this vector's component by a scalar value (v1.x * scalar, v1.y * scalar)
+    Vector2& operator*=(float scalar)
+    {
+        x *= scalar;
+        y *= scalar;
+
+        return *this;
+    }
+
+    /// Divides this vector's component by a scalar value (v1.x / scalar, v1.y / scalar)
+    Vector2& operator/=(float scalar)
+    {
+        x /= scalar;
+        y /= scalar;
+
+        return *this;
+    }
 };
