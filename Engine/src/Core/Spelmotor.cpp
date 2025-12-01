fe0f2ac08533a7245aa894ced0b51c6c7d61af58
Sirbarry <lieven3.schokker@gmail.com>
2025-12-01 14:58:24 +0100

    Fix: merge

--- Engine/src/Core/Spelmotor.cpp
+++ Engine/src/Core/Spelmotor.cpp
@@ -9,22 +9,23 @@
 
 
 SpelMotor::SpelMotor(ApplicationSpecifications const applicationSpecifications)
-    : running(false),
-      specifications(applicationSpecifications),
-      timer(nullptr),
-      tickRate(applicationSpecifications.tickRate)
+	: running(false),
+	  specifications(applicationSpecifications),
+	  timer(nullptr),
+	  tickRate(applicationSpecifications.tickRate)
 {
-    if (applicationSpecifications.renderBackend == RenderBackend::SDL)
-    {
-        SdlContext context = SdlContext();
-        timer.reset();
-        timer = std::make_unique<ApplicationClock>(1.0f / tickRate, []() {
-            // Get Ticks returns ms we need seconds;
-            return (SDL_GetTicks() / 1000.0);
-        });
-
-        renderer = std::make_unique<SDLRenderer>(context);
-    }
+	if (applicationSpecifications.renderBackend == RenderBackend::SDL)
+	{
+		SdlContext context = SdlContext();
+		timer.reset();
+		timer = std::make_unique<ApplicationClock>(1.0f / tickRate, []()
+		{
+			// Get Ticks returns ms we need seconds;
+			return (SDL_GetTicks() / 1000.0);
+		});
+		renderer = std::make_unique<SDLRenderer>(context);
+	}
+	physicsWorld = std::make_unique<Box2DPhysicsWorld>();
 }
 
 
@@ -32,54 +33,54 @@ SpelMotor::~SpelMotor() = default;
 
 void SpelMotor::start()
 {
-    timer->start();
+	timer->start();
 
-    // TODO Server or Client -> Start()
+	// TODO Server or Client -> Start()
 	physicsWorld->start();
-    // TODO SceneManager -> Start()
+	// TODO SceneManager -> Start()
 
-    renderer->open(specifications.windowOptions);
+	renderer->open(specifications.windowOptions);
 
-    run();
+	run();
 }
 
 
 void SpelMotor::run()
 {
-    running = true;
+	running = true;
 
-    InputManager* input = InputManager::getInstance();
+	InputManager* input = InputManager::getInstance();
 
-    while (running)
-    {
-        timer->tick();
+	while (running)
+	{
+		timer->tick();
 
-        if (input->quitRequested())
-        {
-            running = false;
-        }
+		if (input->quitRequested())
+		{
+			running = false;
+		}
 
-        while (timer->shouldFixedUpdate())
-        {
-            input->update();
+		while (timer->shouldFixedUpdate())
+		{
+			input->update();
 			physicsWorld->fixedUpdate();
-            timer->consumeFixedUpdate();
-        }
+			timer->consumeFixedUpdate();
+		}
 
-        // TODO Network->Update()
-        // TODO Audio->Update();
-        renderer->presentFrame();
-    }
+		// TODO Network->Update()
+		// TODO Audio->Update();
+		renderer->presentFrame();
+	}
 }
 
 
 void SpelMotor::shutdown()
 {
-    running = false;
+	running = false;
 
-    // TODO audioSystem->shutdown()
-    InputManager::shutdown();
-    renderer->close();
-    // TODO scenemanager->shutdown()
-    // TODO server->shutdown() and client->shutdown()
+	// TODO audioSystem->shutdown()
+	InputManager::shutdown();
+	renderer->close();
+	// TODO scenemanager->shutdown()
+	// TODO server->shutdown() and client->shutdown()
 }
