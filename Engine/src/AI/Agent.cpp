28fd5a998207b65c5c202735832f206a888c363d
SAMBA-PC\samle <sam.leguijt@gmail.com>
2025-12-02 23:49:55 +0100

    Implemented Agent update method, moving the transform of its gameobject based on module forces

--- Engine/src/AI/Agent.cpp
+++ Engine/src/AI/Agent.cpp
@@ -3,7 +3,6 @@
 //
 
 #include "AI/Agent.h"
-
 #include "Component/Transform.h"
 
 void Agent::onAwake()
@@ -20,13 +19,19 @@ void Agent::onAwake()
 void Agent::update()
 {
     Vector2 velocity = computeDesiredVelocity();
-
     transform->setPosition(transform->getPosition() + velocity);
 }
 
 Vector2 Agent::computeDesiredVelocity()
 {
-    return Vector2::zero();
+    Vector2 totalModulesForce = computeModuleForce();
+
+    if (totalModulesForce.magnitude() > maxVelocityMagnitude)
+    {
+        totalModulesForce = totalModulesForce.normalised() * maxVelocityMagnitude;
+    }
+
+    return totalModulesForce;
 }
 
 Vector2 Agent::computeModuleForce()
@@ -34,19 +39,24 @@ Vector2 Agent::computeModuleForce()
     Vector2 totalForce = Vector2::zero();
     Vector2 accumulatedForce = Vector2::zero();
 
+    /// TODO: Add a data class that caches the Module and its weight instead of lookups every frame.
     for (std::unique_ptr<BaseAgentModule>& modulePtr : modules)
     {
         BaseAgentModule* module = modulePtr.get();
         float weight = moduleWeights[module->getModuleType()];
         Vector2 force = module->compute();
 
-        accumulatedForce += force * weight;
-    }
 
-    if (accumulatedForce.magnitude() > maxModuleForceMagnitude)
-    {
-        accumulatedForce.normalize();
-        accumulatedForce *= maxModuleForceMagnitude;
+        accumulatedForce += force * weight;
+
+        /// Make sure the accumulated force vector doesnt exceed the max allowed magnitude.
+        if (accumulatedForce.magnitude() > maxModuleForceMagnitude)
+        {
+            accumulatedForce = accumulatedForce.normalised() * maxModuleForceMagnitude;
+        }
+
+        /// Add the current iterated module computation to the total force.
+        totalForce += accumulatedForce;
     }
 
     return totalForce;
