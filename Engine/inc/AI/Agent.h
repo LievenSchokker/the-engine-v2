6d3af1e467d281a0695ce4f0a50057614ccb607f
SAMBA-PC\samle <sam.leguijt@gmail.com>
2025-12-02 11:44:02 +0100

    Added Agent class in its own directory

---
+++ Engine/inc/AI/Agent.h
@@ -0,0 +1,39 @@
+//
+// Created by samle on 02/12/2025.
+//
+
+
+#pragma once
+
+
+#include "Behaviour/Behaviour.h"
+#include <map>
+#include <unordered_map>
+
+#include "GameObject/Vector2.h"
+struct Vector2;
+
+class Agent final : public Behaviour
+{
+    public:
+        Agent() :
+            currentVelocity(Vector2{0,0}), maxMotionMagnitude(0), maxSpeed(0) {};
+        ~Agent() override = default;
+
+        void update() override;
+        Vector2 computeModuleForce();
+        Vector2 computeDesiredVelocity();
+
+        bool addAgentModule(); /// Todo: add module param
+        bool removeAgentModule(); /// Todo: add module param
+        bool setModuleWeight(float desiredWeight); // Todo: add module param
+        bool setModuleStatus(); /// Todo: add module param, add moduleStatus param
+
+    private:
+        std::vector<Vector2> modulePositions; /// Todo: replace vector2 with ModuleData
+        std::unordered_map<Vector2, float> moduleWeights; /// Todo: Replace vector2 with moduleType
+        Vector2 currentVelocity;
+        float maxMotionMagnitude;
+        float maxSpeed;
+};
+
---
+++ Engine/src/AI/Agent.cpp
@@ -0,0 +1,6 @@
+//
+// Created by samle on 02/12/2025.
+//
+
+
+#include "AI/Agent.h"
