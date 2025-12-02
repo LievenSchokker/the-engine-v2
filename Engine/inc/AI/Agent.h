2018cb38f79a1c876041fe3bfc0e12a4f5519caf
SAMBA-PC\samle <sam.leguijt@gmail.com>
2025-12-02 23:06:07 +0100

    WiP Agent cpp implementation, need to merge current Vector2 implementation lol

--- Engine/inc/AI/Agent.h
+++ Engine/inc/AI/Agent.h
@@ -14,12 +14,13 @@ struct Vector2;
 class Agent final : public Behaviour
 {
     public:
-        Agent() : currentVelocity(Vector2{0, 0}), maxModuleForceMagnitude(0), maxVelocityMagnitude(0)
+        Agent() : transform(nullptr),currentVelocity(Vector2{0, 0}), maxModuleForceMagnitude(0), maxVelocityMagnitude(0)
         {
         };
 
         ~Agent() override = default;
 
+        void onAwake() override;
         void update() override;
 
         Vector2 computeModuleForce();
@@ -40,15 +41,16 @@ class Agent final : public Behaviour
         template<typename t>
         [[nodiscard]] bool hasAgentModule() const;
 
-        float getMaxModuleForceMagnitude();
+        float getMaxModuleForceMagnitude() const;
         void setMaxModuleForceMagnitude(float max);
-        float getMaxVelocityMagnitude();
+        float getMaxVelocityMagnitude() const;
         void setMaxVelocityMagnitude(float max);
 
     private:
         template<typename T>
         [[nodiscard]] BaseAgentModule* getAgentModule() const;
 
+        Transform* transform;
         std::vector<std::unique_ptr<BaseAgentModule> > modules;
         std::unordered_map<ModuleType, float> moduleWeights;
         Vector2 currentVelocity;
--- Engine/src/AI/Agent.cpp
+++ Engine/src/AI/Agent.cpp
@@ -4,21 +4,56 @@
 
 #include "AI/Agent.h"
 
+#include "Component/Transform.h"
+
+void Agent::onAwake()
+{
+    transform = gameObject->getTransform();
+
+    if (transform == nullptr)
+    {
+        setEnabled(false);
+    }
+}
+
 
 void Agent::update()
 {
+    Vector2 velocity = computeDesiredVelocity();
 
+    // transform->setPosition(transform->getPosition() + velocity);
 }
 
 Vector2 Agent::computeDesiredVelocity()
 {
-    return Vector2{0,0};
+
 }
 
 Vector2 Agent::computeModuleForce()
 {
     return Vector2{0,0};
 }
+
+float Agent::getMaxModuleForceMagnitude() const
+{
+    return maxModuleForceMagnitude;
+}
+
+void Agent::setMaxModuleForceMagnitude(float max)
+{
+    maxModuleForceMagnitude = max;
+}
+
+float Agent::getMaxVelocityMagnitude() const
+{
+    return maxVelocityMagnitude;
+}
+
+void Agent::setMaxVelocityMagnitude(float max)
+{
+    maxVelocityMagnitude = max;
+}
+
 
 
 
