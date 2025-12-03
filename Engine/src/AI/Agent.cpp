e0ed3e4928dd04cf5945532664ea4f8fa2b39572
SAMBA-PC\samle <sam.leguijt@gmail.com>
2025-12-03 12:22:42 +0100

    Changes: - Agent update uses ModuleDatas isntead of previous implementation

--- Engine/src/AI/Agent.cpp
+++ Engine/src/AI/Agent.cpp
@@ -39,25 +39,26 @@ Vector2 Agent::computeModuleForce()
     Vector2 totalForce = Vector2::zero();
     Vector2 accumulatedForce = Vector2::zero();
 
-    /// TODO: Add a data class that caches the Module and its weight instead of lookups every frame.
-    // for (std::unique_ptr<BaseAgentModule>& modulePtr : modules)
-    // {
-    //     BaseAgentModule* module = modulePtr.get();
-    //     float weight = moduleWeights[module->getModuleType()];
-    //     Vector2 force = module->compute();
-    //
-    //
-    //     accumulatedForce += force * weight;
-    //
-    //     /// Make sure the accumulated force vector doesnt exceed the max allowed magnitude.
-    //     if (accumulatedForce.magnitude() > maxModuleForceMagnitude)
-    //     {
-    //         accumulatedForce = accumulatedForce.normalised() * maxModuleForceMagnitude;
-    //     }
-    //
-    //     /// Add the current iterated module computation to the total force.
-    //     totalForce += accumulatedForce;
-    // }
+    for (std::unique_ptr<ModuleData>& moduleData : moduleDatas)
+    {
+        float weight = moduleData->getWeight();
+
+        /// Skip computation for 0 weights.
+        if (weight <= 0)
+            continue;
+
+        Vector2 force = moduleData->getModule()->compute();
+        accumulatedForce += force * weight;
+
+        /// Make sure the accumulated force vector doesnt exceed the max allowed magnitude.
+        if (accumulatedForce.magnitude() > maxModuleForceMagnitude)
+        {
+            accumulatedForce = accumulatedForce.normalised() * maxModuleForceMagnitude;
+        }
+
+        /// Add the current iterated module computation to the total force.
+        totalForce += accumulatedForce;
+    }
 
     return totalForce;
 }
