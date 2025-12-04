c24aa58613feb9719b3ab4e695a32b675c707a1b
SAMBA-PC\samle <sam.leguijt@gmail.com>
2025-12-04 15:36:27 +0100

    REmoved previously added includes to sandbox main cpp

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -2,26 +2,11 @@
 // Created by Lieven Schokker on 07/11/2025.
 //
 
-#include <iostream>
 
 #include "EntryPoint.h"
 
-#include "External/SdlContext.h"
-#include "Rendering/SDL/SDLRenderer.h"
-#include "Rendering/Window/WindowOptions.h"
-#include "Scene/SceneManager.h"
-#include "Component/Transform.h"
-#include "Component/ShapeRenderer.h"
-#include "GameObject/GameObject.h"
-#include "Rendering/Color.h"
-#include "Rendering/RenderQueue.h"
-#include "Input/InputManager.h"
-
-#include <memory>
 #include <iostream>
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
 // This has been added because sometimes SDL causes main to be redefined.
 // Which then causes linking error's
 #undef main
@@ -30,10 +15,4 @@
 int main(int argc, char** argv)
 {
     return SpelMotorEntry::main(argc, argv);
-
-    /// Make game cinstance
-    /// shizzle
-    ///
-    /// SpelMotor->addGame(game);
-    /// SpelMotor->run();
 }
