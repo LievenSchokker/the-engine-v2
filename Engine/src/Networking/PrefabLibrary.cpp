254b3acb83e26f74243ef47cf5c7935cc070db94
Sirbarry <lieven3.schokker@gmail.com>
2025-12-11 15:23:11 +0100

    Feat: removed name from prefabLibrary.cpp

--- Engine/src/Networking/PrefabLibrary.cpp
+++ Engine/src/Networking/PrefabLibrary.cpp
@@ -7,26 +7,14 @@
 
 uint32_t PrefabLibrary::add(std::unique_ptr<GameObject> prefab)
 {
-    if (!prefab)
-    {
-        return 0;
-    }
+    if (prefab == nullptr) return 0;
 
     uint32_t assetId = nextAssetId++;
-    std::string name = prefab->getName();
 
-    // Check if has NetworkIdentity (is a network prefab)
     if (prefab->getComponent<NetworkIdentity>())
     {
         networkPrefabIds.push_back(assetId);
     }
-
-    // Store name mapping
-    if (!name.empty())
-    {
-        nameToId[name] = assetId;
-    }
-
 
     prefabsById[assetId] = std::move(prefab);
     return assetId;
@@ -52,10 +40,6 @@ void PrefabLibrary::add(uint32_t assetId, std::unique_ptr<GameObject> prefab)
         networkPrefabIds.push_back(assetId);
     }
 
-    if (!name.empty())
-    {
-        nameToId[name] = assetId;
-    }
 
     prefabsById[assetId] = std::move(prefab);
 }
@@ -76,34 +60,10 @@ std::unique_ptr<GameObject> PrefabLibrary::instantiate(uint32_t assetId) const
 	return it->second->clone();
 }
 
-std::unique_ptr<GameObject> PrefabLibrary::instantiate(const std::string& name) const
-{
-    auto it = nameToId.find(name);
-    if (it == nameToId.end())
-    {
-        return nullptr;
-    }
-
-    return instantiate(it->second);
-}
-
 GameObject* PrefabLibrary::getTemplate(uint32_t assetId) const
 {
     auto it = prefabsById.find(assetId);
     return (it != prefabsById.end()) ? it->second.get() : nullptr;
-}
-
-GameObject* PrefabLibrary::getTemplate(const std::string& name) const
-{
-    auto it = nameToId.find(name);
-    if (it == nameToId.end()) return nullptr;
-    return getTemplate(it->second);
-}
-
-uint32_t PrefabLibrary::getAssetId(const std::string& name) const
-{
-    auto it = nameToId.find(name);
-    return (it != nameToId.end()) ? it->second : 0;
 }
 
 const std::vector<uint32_t>& PrefabLibrary::getNetworkPrefabIds() const
@@ -114,11 +74,6 @@ const std::vector<uint32_t>& PrefabLibrary::getNetworkPrefabIds() const
 bool PrefabLibrary::contains(uint32_t assetId) const
 {
     return prefabsById.find(assetId) != prefabsById.end();
-}
-
-bool PrefabLibrary::contains(const std::string& name) const
-{
-    return nameToId.find(name) != nameToId.end();
 }
 
 size_t PrefabLibrary::size() const
