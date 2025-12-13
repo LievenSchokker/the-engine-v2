3091a278b929ba7110bbf37e86dd8c820d0a7cb4
Sirbarry <lieven3.schokker@gmail.com>
2025-12-13 13:34:46 +0100

    Fix: added a window close event

--- Engine/src/Core/EngineLoops/ClientLoop.cpp
+++ Engine/src/Core/EngineLoops/ClientLoop.cpp
@@ -86,6 +86,13 @@ void ClientLoop::initializeEvents()
 			}
 		});
 
+    eventDispatcher->subscribe<WindowCloseEvent>(
+    [this](const WindowCloseEvent& event)
+    {
+        shutdown();
+    });
+
+
 	std::cout << "Setting up events" << std::endl;
 	renderer->setupEvents(*eventDispatcher.get());
 }
