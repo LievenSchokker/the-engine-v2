8338b22a26e3c4c037553a3d51d80e2064803a8b
samleguijt <sam.leguijt@gmail.com>
2025-12-13 11:51:31 +0100

    Cleaned up Utility direction classes

--- Engine/inc/AI/Navigation/CardinalDirections.h
+++ Engine/inc/AI/Navigation/CardinalDirections.h
@@ -3,14 +3,23 @@
 
 
 struct Vector2;
-
 #include <vector>
 
 
+/**
+ * @brief Utility class for getting the Cardinal Directions (NESW) contained in a std::vector.
+ *
+ * Stores each direction as a normalized 2D unit Vector2:
+ * Directions are stored in order:
+ * - Up (0, 1)
+ * - Right (1,0)
+ * - Down (0, -1)
+ * - Left (-1,0)
+ */
 class CardinalDirections
 {
     public:
-        static const std::vector<Vector2>& getDirections();
+    static const std::vector<Vector2>& getDirections();
 
     private:
         static const std::vector<Vector2> directions;
