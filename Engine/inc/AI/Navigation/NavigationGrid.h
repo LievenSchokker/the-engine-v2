8612c02b36a957eb4f77a653fa8acd082417450e
samleguijt <sam.leguijt@gmail.com>
2025-12-13 14:23:57 +0100

    Cleaned up NavigationGrid

--- Engine/inc/AI/Navigation/NavigationGrid.h
+++ Engine/inc/AI/Navigation/NavigationGrid.h
@@ -5,13 +5,20 @@
 #pragma once
 
 
-#include "INavigationSurface.h"
-#include "NavigationGridGraphAdapter.h"
 #include "AI/Navigation/GridCell.h"
-#include "Component/BaseComponentTypes/RenderComponent.h"
-#include "Pathfinding/IPathfindingGraph.h"
+#include "INavigationSurface.h"
 #include "Math/Vector2.h"
+#include "NavigationGridGraphAdapter.h"
+#include "Pathfinding/IPathfindingGraph.h"
 
+#include <memory>
+
+
+/**
+ * @brief Concrete implementation of a INavigationSurface, representing a basic 2D grid of @c GridCell
+ *
+ * This class only stores the data needed to represent a grid, stored in a flattened std::vector instead of a 2D array.
+ */
 class NavigationGrid : public INavigationSurface
 {
     public:
@@ -23,40 +30,129 @@ class NavigationGrid : public INavigationSurface
 
         ~NavigationGrid() override = default;
 
-        void generateGrid(Vector2 gridDimensions, Vector2 cellSize);
-
-        bool isWalkable(Vector2 cellPosition) const;
-        void setWalkable(Vector2 cellPosition, bool value);
-
-        int getCellWeight(Vector2 cellPosition) const;
-        void setCellWeight(Vector2 cellPosition, float weight);
-
-        Vector2 getCellSize() const;
-        void setCellSize(Vector2 size);
-
-        bool isInGrid(Vector2 cellPosition) const;
-
-        Vector2 worldToCellPosition(Vector2 worldPos) const;
-        Vector2 cellToWorldPosition(Vector2 cellPos) const;
-
-        const std::vector<GridCell>& getCells() const;
-
-        int getWidth() const;
-        int getHeight() const;
-
-        std::vector<Vector2> getNeighbours(Vector2 cellPos, bool diagonalNeighbours) const;
-        GridCell& getNavigationCell(Vector2 positionInGrid);
-
+        /// Interface implementation:
         const IPathfindingGraph* getPathfindingGraph() const override;
         void bakeSurface(const std::vector<BoundingBox>& obstacles)  override;
         Vector2 toWorldPoint(Vector2 surfacePoint) const override;
         Vector2 toSurfacePoint(Vector2 worldPoint) const override;
         bool isPointOnSurface(Vector2 surfacePoint) const override;
 
+        /**
+         * @brief creates a new grid by using the dimensions for its size, filling the @c cells with Vector2's
+         * @param gridDimensions Widht and Height of the grid, represented in a Vector2. Gets rounded down to integer values
+         * @param cellSize Size each cell in the grid should be.
+         */
+        void generateGrid(Vector2 gridDimensions, Vector2 cellSize);
+
+        /**
+        * @brief Converts a given world position to a position in the grid
+        * @param worldPos The position to convert
+        * @return The converted position
+        */
+        Vector2 worldToCellPosition(Vector2 worldPos) const;
+
+        /**
+         * @brief Converts a given position from the grid to world space
+         * @param cellPos the position of the cell to convert
+         * @return The position in world space
+         */
+        Vector2 cellToWorldPosition(Vector2 cellPos) const;
+
+        /**
+         * @brief Retrieves the @c GridCell existing on the provided position in the grid
+         * @param positionInGrid position to retrieve the cell from
+         * @return a Reference to the retrieved GridCell
+         */
+        GridCell& getCellAt(Vector2 positionInGrid);
+
+        /**
+         * @brief Retrieves the position of a given cell in the grid
+         * @param cell the position to return of
+         * @return The position in the grid. use @c cellToWorldPosition() to convert to world space coordinates.
+         */
+        Vector2 getCellPosition(const GridCell& cell) const;
+
+        /**
+         * @brief Checks whether a cell at the provided position exists within the grid
+         * @param cellPosition Cell to look for in the grid
+         * @return true if the provided position is in the grid, false otherwise
+         */
+        bool isInGrid(Vector2 cellPosition) const;
+
+        /**
+         * @brief Retrieves a reference to all the GridCells in this grid
+         * @return a const reference vector of GridCells
+         */
+        const std::vector<GridCell>& getCells() const;
+
+        /**
+         * Retrieves every neighbour cell from the provided cell
+         *
+         * Can be used to get neighbours in 4 (NESW) or 8 (NESW + diagonals) directions
+         * @param origin The position of the cell to look for neighbours from
+         * @param diagonalNeighbours Should diagonal neighbours be included?
+         * @return a vector<Vector2> containing each neighbour of the origin cell
+         */
+        std::vector<Vector2> getNeighbours(Vector2 origin, bool diagonalNeighbours) const;
+
+        /**
+         * @brief Checks whether the given cell in the grid isWalkable
+         * @param cellPosition cell in the grid to check
+         * @return true if walkable, false otherwise
+         */
+        bool isWalkable(Vector2 cellPosition) const;
+
+        /**
+         * Sets the isWalkable member of an @c GridCell to the provided value
+         * @param cellPosition cell in the grid to set
+         * @param value the new value of cell's isWalkable
+         */
+        void setWalkable(Vector2 cellPosition, bool value);
+
+        /**
+         * Retrieves teh weight member of GridCell at the given position
+         * @param cellPosition
+         * @return the weight
+         */
+        int getCellWeight(Vector2 cellPosition) const;
+
+        /**
+         * @brief Sets the weight member of the GridCell at the provided position in the grid
+         * @param cellPosition cell in the grid to set
+         * @param weight the new weight of the cell
+         */
+        void setCellWeight(Vector2 cellPosition, float weight);
+
+        /**
+         * @brief Retrieves the size of cells used for this grid
+         * @return A vector2 containing the Widht and Height
+         */
+        Vector2 getCellSize() const;
+
+        /**
+         * @brief Sets the size of cells in the grid
+         * @param size the desired size
+         */
+        void setCellSize(Vector2 size);
+
+        /**
+         * Retrieves the width of this grid
+         * @return width
+         */
+        int getWidth() const;
+
+        /**
+         * @brief Retrieves the height of this grid
+         * @return height
+         */
+        int getHeight() const;
+
     private:
         int width;
         int height;
         Vector2 cellSize;
+
+        /// Cells are stored in a vector to increase efficiency
         std::vector<GridCell> cells;
         std::unique_ptr<IPathfindingGraph> graphAdapter;
 };
--- Engine/src/AI/Navigation/NavigationGrid.cpp
+++ Engine/src/AI/Navigation/NavigationGrid.cpp
@@ -5,140 +5,13 @@
 
 #include "AI/Navigation/NavigationGrid.h"
 
+
 #include "AI/Navigation/BoundingBox.h"
 #include "AI/Navigation/CardinalDirections.h"
 #include "AI/Navigation/CompassDirections.h"
 #include "AI/Navigation/GridCell.h"
 #include "Math/Vector2.h"
-
-
-void NavigationGrid::generateGrid(Vector2 dimensions, Vector2 cellsSize)
-{
-    width = static_cast<int>(dimensions.x);
-    height = static_cast<int>(dimensions.y);
-    cells.resize(width * height);
-
-    cellSize = cellsSize;
-}
-
-bool NavigationGrid::isWalkable(Vector2 cellPosition) const
-{
-    if (!isInGrid(cellPosition))
-        return false;
-
-    return cells[cellPosition.y * width + cellPosition.x].walkable;
-}
-
-int NavigationGrid::getCellWeight(Vector2 cellPosition) const
-{
-    if (!isInGrid(cellPosition))
-        return 0;
-
-    return cells[cellPosition.y * width + cellPosition.x].weight;
-}
-
-
-void NavigationGrid::setWalkable(Vector2 cellPosition, bool value)
-{
-    if (!isInGrid(cellPosition))
-        return ;
-
-     cells[cellPosition.y * width + cellPosition.x].walkable = value;
-}
-
-
-void NavigationGrid::setCellWeight(Vector2 cellPosition, float weight)
-{
-    if (!isInGrid(cellPosition))
-        return;
-
-    cells[cellPosition.y * width + cellPosition.x].weight = weight;
-}
-
-
-Vector2 NavigationGrid::getCellSize() const
-{
-    return cellSize;
-}
-
-
-void NavigationGrid::setCellSize(Vector2 size)
-{
-    cellSize = size;
-}
-
-
-Vector2 NavigationGrid::worldToCellPosition(Vector2 worldPos) const
-{
-    int x = int(worldPos.x / cellSize.x);
-    int y = int(worldPos.y / cellSize.y);
-
-    x = std::clamp(x, 0, width - 1);
-    y = std::clamp(y, 0, height - 1);
-
-    return Vector2{static_cast<float>(x), static_cast<float>(y) };
-}
-
-
-Vector2 NavigationGrid::cellToWorldPosition(Vector2 cellPos) const
-{
-    float worldX = cellPos.x * cellSize.x + cellSize.x * 0.5f;
-    float worldY = cellPos.y * cellSize.y + cellSize.y * 0.5f;
-
-    return Vector2{ worldX, worldY };
-}
-
-const std::vector<GridCell> &NavigationGrid::getCells() const
-{
-    return cells;
-}
-
-
-bool NavigationGrid::isInGrid(Vector2 position) const
-{
-    int x = static_cast<int>(position.x);
-    int y = static_cast<int>(position.y);
-
-    return x >= 0 && y >= 0 && x < width && y < height;
-}
-
-int NavigationGrid::getWidth() const
-{
-    return width;
-}
-
-int NavigationGrid::getHeight() const
-{
-    return height;
-}
-
-
-std::vector<Vector2> NavigationGrid::getNeighbours(Vector2 cellPos, bool diagonalNeighbours) const
-{
-    std::vector<Vector2> validNeighbours;
-
-    std::vector<Vector2> directions;
-    if (diagonalNeighbours)
-        directions = CompassDirections::getDirections();
-    else
-        directions = CardinalDirections::getDirections();
-
-    for (Vector2 direction : directions)
-    {
-        Vector2 neighbour = cellPos + direction;
-
-        if (isInGrid(neighbour))
-            validNeighbours.push_back(neighbour);
-    }
-
-    return validNeighbours;
-}
-
-GridCell& NavigationGrid::getNavigationCell(Vector2 positionInGrid)
-{
-    int index = static_cast<int>(positionInGrid.y) * width + static_cast<int>(positionInGrid.x);
-    return cells[index];
-}
+#include <algorithm>
 
 
 const IPathfindingGraph* NavigationGrid::getPathfindingGraph() const
@@ -170,17 +43,169 @@ void NavigationGrid::bakeSurface(const std::vector<BoundingBox>& obstacles)
     }
 }
 
+
 Vector2 NavigationGrid::toSurfacePoint(Vector2 worldPoint) const
 {
     return worldToCellPosition(worldPoint);
 }
+
 
 Vector2 NavigationGrid::toWorldPoint(Vector2 surfacePoint) const
 {
     return cellToWorldPosition(surfacePoint);
 }
 
+
 bool NavigationGrid::isPointOnSurface(Vector2 surfacePoint) const
 {
     return isInGrid(surfacePoint);
 }
+
+
+void NavigationGrid::generateGrid(Vector2 dimensions, Vector2 cellsSize)
+{
+    width = std::floor(cellsSize.x);
+    height = std::floor(dimensions.y);
+
+    cells.resize(width * height);
+    cellSize = cellsSize;
+}
+
+
+Vector2 NavigationGrid::worldToCellPosition(Vector2 worldPos) const
+{
+    int x = int(worldPos.x / cellSize.x);
+    int y = int(worldPos.y / cellSize.y);
+
+    x = std::clamp(x, 0, width - 1);
+    y = std::clamp(y, 0, height - 1);
+
+    return Vector2{static_cast<float>(x), static_cast<float>(y) };
+}
+
+
+Vector2 NavigationGrid::cellToWorldPosition(Vector2 cellPos) const
+{
+    float worldX = cellPos.x * cellSize.x + cellSize.x * 0.5f;
+    float worldY = cellPos.y * cellSize.y + cellSize.y * 0.5f;
+
+    return Vector2{ worldX, worldY };
+}
+
+
+GridCell& NavigationGrid::getCellAt(Vector2 positionInGrid)
+{
+    int index = static_cast<int>(positionInGrid.y) * width + static_cast<int>(positionInGrid.x);
+    return cells[index];
+}
+
+
+Vector2 NavigationGrid::getCellPosition(const GridCell& cell) const
+{
+    int index = &cell - &cells[0];
+    return Vector2{ float(index % width), float(index / width) };
+}
+
+
+bool NavigationGrid::isInGrid(Vector2 position) const
+{
+    int x = static_cast<int>(position.x);
+    int y = static_cast<int>(position.y);
+
+    return x >= 0 && y >= 0 && x < width && y < height;
+}
+
+
+const std::vector<GridCell> &NavigationGrid::getCells() const
+{
+    return cells;
+}
+
+
+std::vector<Vector2> NavigationGrid::getNeighbours(Vector2 cellPos, bool diagonalNeighbours) const
+{
+    std::vector<Vector2> validNeighbours;
+
+    std::vector<Vector2> directions;
+    if (diagonalNeighbours)
+        directions = CompassDirections::getDirections();
+    else
+        directions = CardinalDirections::getDirections();
+
+    for (Vector2 direction : directions)
+    {
+        Vector2 neighbour = cellPos + direction;
+
+        if (isInGrid(neighbour))
+            validNeighbours.push_back(neighbour);
+    }
+
+    return validNeighbours;
+}
+
+
+bool NavigationGrid::isWalkable(Vector2 cellPosition) const
+{
+    if (!isInGrid(cellPosition))
+        return false;
+
+    return cells[cellPosition.y * width + cellPosition.x].walkable;
+}
+
+
+void NavigationGrid::setWalkable(Vector2 cellPosition, bool value)
+{
+    if (!isInGrid(cellPosition))
+        return ;
+
+    cells[cellPosition.y * width + cellPosition.x].walkable = value;
+}
+
+
+int NavigationGrid::getCellWeight(Vector2 cellPosition) const
+{
+    if (!isInGrid(cellPosition))
+        return 0;
+
+    return cells[cellPosition.y * width + cellPosition.x].weight;
+}
+
+
+void NavigationGrid::setCellWeight(Vector2 cellPosition, float weight)
+{
+    if (!isInGrid(cellPosition))
+        return;
+
+    cells[cellPosition.y * width + cellPosition.x].weight = weight;
+}
+
+
+Vector2 NavigationGrid::getCellSize() const
+{
+    return cellSize;
+}
+
+
+void NavigationGrid::setCellSize(Vector2 size)
+{
+    cellSize = size;
+}
+
+
+int NavigationGrid::getWidth() const
+{
+    return width;
+}
+
+
+int NavigationGrid::getHeight() const
+{
+    return height;
+}
+
+
+
+
+
+
+
