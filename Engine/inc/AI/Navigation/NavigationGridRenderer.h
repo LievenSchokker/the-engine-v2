e86d6b5c99d3666493828dc0d12735df7897645b
samleguijt <sam.leguijt@gmail.com>
2025-12-15 15:44:24 +0100

    Added gridrender options struct

--- Engine/inc/AI/Navigation/NavigationGridRenderer.h
+++ Engine/inc/AI/Navigation/NavigationGridRenderer.h
@@ -3,14 +3,24 @@
 //
 
 #pragma once
+
+
 #include "NavigationGrid.h"
 #include "Behaviour/Behaviour.h"
 #include "Component/BaseComponentTypes/RenderComponent.h"
 
+struct GridRenderOptions
+{
+    float lineThickness = 1.0f;
+    Color walkableCellsColor = Color::darkGreen();
+    Color unwalkableCellsColor = Color::darkRed();
+};
+
+
 class NavigationGridRenderer : public Behaviour, public RenderComponent
 {
     public:
-        explicit NavigationGridRenderer() : grid(nullptr) {};
+        explicit NavigationGridRenderer() : grid(nullptr), gridRenderOptions(1.0f, Color::darkGreen(), Color::darkRed()){};
 
         void onAwake() override;
 
@@ -21,9 +31,10 @@ class NavigationGridRenderer : public Behaviour, public RenderComponent
 
     private:
         NavigationGrid* grid;
+        GridRenderOptions gridRenderOptions;
 
-    std::vector<RenderCommand> cachedCommands;
-    bool cacheBuilt = false;
+        std::vector<RenderCommand> cachedCommands;
+        bool cacheBuilt = false;
 
     void buildCache();
 };
