057bd0312ce38ba66db217333fe02058bbc22e0e
Thijs Teerhuis <thijs@teerhuis.eu>
2025-12-17 21:01:39 +0100

    Removed relative imports

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,5 +1,5 @@
 
-#include "../Engine/inc/Core/Options/ApplicationSpecifications.h"
+#include "Core/Options/ApplicationSpecifications.h"
 #include "EntryPoint.h"
 #include "Game.h"
 #include "GameObject/GameObject.h"
--- Tests/Core/SpelMotorTest.cpp
+++ Tests/Core/SpelMotorTest.cpp
@@ -1,4 +1,3 @@
-// Tests/Core/SpelMotorTest.cpp
 #include "Game.h"
 
 #include <gtest/gtest.h>
@@ -6,7 +5,7 @@
 #include <chrono>
 #include <SDL2/SDL.h>
 #include "Core/SpelMotor.h"
-#include "../../Engine/inc/Core/Options/ApplicationSpecifications.h"
+#include "Core/Options/ApplicationSpecifications.h"
 
 class SpelMotorTest : public ::testing::Test
 {
