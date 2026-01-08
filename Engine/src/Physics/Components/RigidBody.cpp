e2722544b980a4ed5bcdc2efcacca2337d5dfd56
Pablo Sanderman <pablosanderman@gmail.com>
2026-01-08 20:02:11 +0100

    Refactor old code

--- Engine/src/Physics/Components/RigidBody.cpp
+++ Engine/src/Physics/Components/RigidBody.cpp
@@ -66,23 +66,6 @@ void RigidBody::onStart()
 	GameWorld* world = getWorld();
 	if ( world != nullptr && world->physics != nullptr )
 	{
-		if ( world->isClient() && !world->isServer() )
-		{
-			auto* identity = getComponent<NetworkIdentity>();
-			if ( identity != nullptr )
-			{
-				const int ownerId = identity->getOwnerId();
-				if ( ownerId < 0 || ownerId != world->localClientId )
-				{
-					if ( bodyType == BodyType::Dynamic )
-					{
-						// Avoid client simulation fighting server snapshots.
-						bodyType = BodyType::Kinematic;
-					}
-				}
-			}
-		}
-
 		world->physics->createBody(this);
 	}
 }
